73eb0d4c4d7c2206183566fcfc21c953cac00b7d
Hamid Rza <hamidrza0008@gmail.com>
2026-05-26 20:28:10 +0530

    fixed vercel build error

--- app/admin/(dashboard)/layout.js
+++ app/admin/(dashboard)/layout.js
@@ -8,7 +8,7 @@ export default function AdminLayout({ children }) {
             <div className="flex-1 flex flex-col">
 
                 {/* Navbar */}
-                <Navbar />
+                <AdminNavbar />
 
                 {/* Main Content */}
                 <main className="flex-1 p-6 overflow-y-auto">
